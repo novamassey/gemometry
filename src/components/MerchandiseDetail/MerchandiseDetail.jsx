4035f011979e10293b8dbfeeacdf87d03329b4a8
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-02-07 14:09:17 -0800

    link is working

---
+++ src/components/MerchandiseDetail/MerchandiseDetail.jsx
@@ -0,0 +1,17 @@
+// import MerchandiseDetailPage from "../../pages/MerchandiseDetailPage/MerchandiseDetailPage";
+
+// export default function MerchandiseDetail({item}) {
+//     console.log("Merchandise Detail", item)
+//     return (
+//         <div>
+
+//         {item}
+//         {/* {item.description}
+//         <div>
+
+//         {item.img_url_detail}
+//         </div> */}
+        
+//         </div>
+//     )
+// }
--- src/components/MerchandiseListItem/MerchandiseListItem.jsx
+++ src/components/MerchandiseListItem/MerchandiseListItem.jsx
@@ -6,8 +6,9 @@ export default function MerchandiseListItem({item, key}){
     return (
     <>
     <div className ="imageCard">
-
-        <div>{item.name}</div>
+        <Link to={`/items/${item._id}`}>
+            <div>{item.name}</div>
+        </Link>
         <div>${item.price}</div>
         <div className ="imageCard" style={{background: `url(${item.img_url_detail})`, backgroundSize: "cover"}}></div>
     </div>
