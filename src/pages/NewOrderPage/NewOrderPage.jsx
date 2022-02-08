7e80cf7ff8ba5d48740c084c694511eb3e03c364
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-02-08 15:02:07 -0800

    order cart works

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -5,20 +5,22 @@ import * as ordersAPI from "../../utilities/orders-api";
 
 export default function NewOrderPage() {
     let orderItems;
-    const [cart, setCart] = useState({})
+    const [cart, setCart] = useState({lineItems:[]})
+    // const [lineItems, setLineItems] = useState({})
     const isMounted = useRef(true);
 
     async function getOrder() {
         const cart = await ordersAPI.getCart()
         console.log(cart);
         setCart(cart);
-       
+        // setLineItems(cart.lineItems);
+        // console.log(lineItems);
     }
 
     useEffect(function() {
         if(isMounted.current) {
        getOrder();
-    
+    //    itemFromOrders();
     }
     return () =>{
         isMounted.current = false
@@ -27,7 +29,26 @@ export default function NewOrderPage() {
   
    
 
+//     let items = []
+//     items = cart.lineItems
+//    console.log(items);
+//    const itemFromOrders =items.map(i => {
+//       <>
+//     <div>
+//         {i.qty}
+//     </div>
+    {/* <div>{i.name}</div> */}
+//        </>
+// })
+// async function itemFromOrders() {
+//     let items = await cart.lineItems
+//      orderItems = items.map(i =>{
+//     <div>
 
+//         <div>{i.qty}</div>
+//         </div>
+//      })
+// } 
 return (
     <>
     <div>{cart._id}</div>
@@ -40,3 +61,25 @@ return (
 )
 
 }
+// return (
+//     <>
+//     {async function itemFromOrders() {
+//         let items = await cart.lineItems
+//         orderItems = items.map(i =>{
+//             <div>
+    
+//             <div>{i.qty}</div>
+//             </div>
+//          })}
+//         }    
+//         {itemFromOrders()}
+//         </>
+//     ) 
+// }
+
+// return (
+//     <div>
+//     {orderItems}
+//  </div>
+// ) 
+// }
