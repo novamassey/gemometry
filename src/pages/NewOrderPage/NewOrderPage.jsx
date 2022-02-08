212dd2d3f6d2b5212c052871eab13e8f8f0397b1
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-02-08 14:50:40 -0800

    order detail page works

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -5,22 +5,20 @@ import * as ordersAPI from "../../utilities/orders-api";
 
 export default function NewOrderPage() {
     let orderItems;
-    const [cart, setCart] = useState([])
-    // const [lineItems, setLineItems] = useState({})
+    const [cart, setCart] = useState({})
     const isMounted = useRef(true);
 
     async function getOrder() {
         const cart = await ordersAPI.getCart()
-        // console.log(cart);
+        console.log(cart);
         setCart(cart);
-        // setLineItems(cart.lineItems);
-        // console.log(lineItems);
+       
     }
 
     useEffect(function() {
         if(isMounted.current) {
        getOrder();
-    //    itemFromOrders();
+    
     }
     return () =>{
         isMounted.current = false
@@ -29,55 +27,16 @@ export default function NewOrderPage() {
   
    
 
-//     let items = []
-//     items = cart.lineItems
-//    console.log(items);
-//    const itemFromOrders =items.map(i => {
-//       <>
-//     <div>
-//         {i.qty}
-//     </div>
-    {/* <div>{i.name}</div> */}
-//        </>
-// })
-// async function itemFromOrders() {
-//     let items = await cart.lineItems
-//      orderItems = items.map(i =>{
-//     <div>
 
-//         <div>{i.qty}</div>
-//         </div>
-//      })
-// } 
 return (
     <>
     <div>{cart._id}</div>
     <br/>
     <div>{cart.totalQty}</div>
-    {/* <div>{cart.lineItems}</div> */}
+    <div>{cart.lineItems.map(lineItem =>
+        <p>{lineItem.item.name}</p>
+        )}</div>
     </>
 )
 
 }
-// return (
-//     <>
-//     {async function itemFromOrders() {
-//         let items = await cart.lineItems
-//         orderItems = items.map(i =>{
-//             <div>
-    
-//             <div>{i.qty}</div>
-//             </div>
-//          })}
-//         }    
-//         {itemFromOrders()}
-//         </>
-//     ) 
-// }
-
-// return (
-//     <div>
-//     {orderItems}
-//  </div>
-// ) 
-// }
