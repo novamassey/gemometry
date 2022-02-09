d71d9ae1b7c0b9f90a3166cc80725b414913e7fd
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-02-09 15:45:59 -0800

    images and order data prints to order history page

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -2,8 +2,8 @@ import {checkToken} from '../../utilities/users-service';
 import * as ordersAPI from '../../utilities/orders-api';
 import { useState, useEffect } from "react";
 
-export default function OrderHistoryPage(){
-    const[orderHistory, setOrderHistory] = useState([{}])
+export default function OrderHistoryPage({user}){
+    const[orderHistory, setOrderHistory] = useState([{lineItems:[]}])
 
     useEffect(function() {
         async function getOrders() {
@@ -17,9 +17,20 @@ export default function OrderHistoryPage(){
 
     return (
     
-    <div>
-        <h1>orderHistory</h1>
+        <>
+        <h3>Hello {user.name.toUpperCase()}! Here are your recent orders, THANK YOU FOR SHOPPING WITH US!</h3>
+        {orderHistory.map(orders => 
+        <div>
+        <p>Reference No: {orders.orderId}</p>
+        <p>Quantity: {orders.totalQty}</p>
+        <p>Total: $ {orders.orderTotal}</p>
+        {orders.lineItems.map(items =>
+        <img src={`${items.item.img_url_list}`}></img> 
+            )}
+        </div>
+        )}
+        </>
         
-    </div>
+        
     )
 }
