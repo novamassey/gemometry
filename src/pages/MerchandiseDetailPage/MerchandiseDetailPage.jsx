a35d070247443e18173d4ea25329424db8de1573
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-03-01 08:37:48 -0800

    some mobile design features

--- src/pages/MerchandiseDetailPage/MerchandiseDetailPage.jsx
+++ src/pages/MerchandiseDetailPage/MerchandiseDetailPage.jsx
@@ -1,20 +1,19 @@
-
 import MerchandiseListItem from "../../components/MerchandiseListItem/MerchandiseListItem";
 import "../../utilities/send-request";
-import{useState, useEffect, useRef} from "react"; 
-import {useParams, useNavigate, Link} from "react-router-dom";  
+import { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import * as itemsAPI from "../../utilities/items-api";
 import * as ordersAPI from "../../utilities/orders-api";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import "./MerchandiseDetail.css";
 
-export default function MerchandiseDetailPage({user}) {
+export default function MerchandiseDetailPage({ user }) {
   const [itemSingle, setItemSingle] = useState();
   const [cart, setCart] = useState(null);
   const [image, setImage] = useState(true);
-  const {id} = useParams();
+  const { id } = useParams();
   const isMounted = useRef(true);
-    
+
   const navigate = useNavigate();
 
   async function getItem() {
@@ -23,9 +22,9 @@ export default function MerchandiseDetailPage({user}) {
   }
 
   async function handleAddToCart(itemId) {
-    const cart =  await ordersAPI.addItemCart(itemId);
+    const cart = await ordersAPI.addItemCart(itemId);
     setCart(cart);
-    navigate('/orders/cart');
+    navigate("/orders/cart");
   }
 
   async function handleChangeImage() {
@@ -37,38 +36,54 @@ export default function MerchandiseDetailPage({user}) {
     setCart(cart);
   }
 
-  useEffect(function() {
-    if(isMounted.current) {
+  useEffect(function () {
+    if (isMounted.current) {
       getItem();
-      }
-    return () =>{
-      isMounted.current = false
     }
+    return () => {
+      isMounted.current = false;
+    };
   }, []);
 
-  <NewOrderPage item={itemSingle} cart={cart} getOrder={getOrder}/>
-    
+  <NewOrderPage item={itemSingle} cart={cart} getOrder={getOrder} />;
+
   return (
     <div>
       <div className="MerchandiseDetailPage">
         <div className="DetailText">
           <h2>{itemSingle && itemSingle.name}</h2>
-            <p>{itemSingle && itemSingle.description}</p>
-            <h2>${itemSingle && itemSingle.price}</h2>
+          <p>{itemSingle && itemSingle.description}</p>
+          <h2>${itemSingle && itemSingle.price}</h2>
         </div>
         <div className="DetailImage">
-          {image && <img onClick={handleChangeImage}src={itemSingle && itemSingle.img_url_detail} />}
-          {!image  && <img onClick={handleChangeImage} src={itemSingle && itemSingle.img_url_detail_extra} />}
+          {image && (
+            <img
+              className="DetailImg"
+              onClick={handleChangeImage}
+              src={itemSingle && itemSingle.img_url_detail}
+            />
+          )}
+          {!image && (
+            <img
+              className="DetailImg"
+              onClick={handleChangeImage}
+              src={itemSingle && itemSingle.img_url_detail_extra}
+            />
+          )}
         </div>
-        { user ?
-          <button className="detailButton" onClick={()=>handleAddToCart(itemSingle._id)} >ADD TO  CART</button>
-        :
-          <Link className="LinkDetail" to='/login'>LOGIN/SIGNUP TO ADD TO CART</Link>    
-        }
+        {user ? (
+          <button
+            className="detailButton"
+            onClick={() => handleAddToCart(itemSingle._id)}
+          >
+            ADD TO CART
+          </button>
+        ) : (
+          <Link className="LinkDetail" to="/login">
+            LOGIN/SIGNUP TO ADD TO CART
+          </Link>
+        )}
       </div>
     </div>
-  )
+  );
 }
-
-
-    
